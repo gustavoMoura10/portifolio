d769d9eccd2b62dbcffb0e8c34b55541aa977b49
Gustavo Moura <gustavomoura.dev@gmail.com>
2025-03-11 22:03:58 -0300

    teste

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -35,6 +35,7 @@ export default function Experience() {
                     setOpenIndex={setOpenIndex}
                     slug={'superlogica'}
                 />
+                
                 <Job
                     imageSrc="/experiences/qualicorp.jpeg"
                     imageAlt="Qualicorp"
