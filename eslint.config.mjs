b86c118fa0e4d8a5791d532b49e4a3623ef737fb
Gustavo Moura <gustavomoura.dev@gmail.com>
2025-03-11 21:55:07 -0300

    fix: Tirando implicity

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",  // ou "off" para desabilitar
+      "@typescript-eslint/no-unused-vars": "warn",  // ou "off" para desabilitar
+    },
+  },
 ];
 
 export default eslintConfig;
