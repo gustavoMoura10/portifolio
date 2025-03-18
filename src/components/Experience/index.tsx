d21891a1a0dcc96354cea6687e7128950eb55d59
Gustavo Moura <gustavomoura.dev@gmail.com>
2025-03-18 13:38:22 -0300

    feat: Atualização de experiência e currículo

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -13,7 +13,7 @@ export default function Experience() {
         {
             imageSrc: "/experiences/superlogica.jpeg",
             imageAlt: "Superlogica",
-            functionJob: "Desenvolvedor Back-End Pleno",
+            functionJob: "Analista Desenvolvedor Pleno",
             enterprise: "Superlógica Tecnologias",
             schedule: "Tempo integral",
             from: "Dez 2021",
@@ -21,14 +21,16 @@ export default function Experience() {
             place: "São Paulo, Brasil",
             typeJob: "Remoto",
             descritiption: `
-                Gerenciamento de banco de dados e desenvolvimento de automações para integração de dados de serviços de terceiros no sistema. Além disso, implementei funcionalidades no sistema que permitiram aos próprios usuários realizar a implantação desses dados de forma autônoma, reduzindo a dependência de suporte técnico e otimizando o fluxo operacional. Também foi implementada soluções de IA para análise de padrões de sistemas terceiros, mapeando automaticamente esses dados ao nosso padrão e aprimorando a eficiência da integração.
+                Gerenciei bancos de dados e desenvolvi automações para integração de dados de serviços de terceiros, utilizando Node.js (JavaScript, TypeScript) e Python para extrair informações em XML, SQL, Excel e JSON, adaptando-as a um formato padronizado. Para otimizar a importação, implementei um fluxo baseado em AWS (EC2, Lambdas e SQS), permitindo processamento em lote e execuções paralelas, garantindo eficiência e evitando gargalos no banco de dados. No front-end, utilizei Angular para criar interfaces intuitivas que facilitam a visualização e o gerenciamento dos formatos de importação.
+                
+                Além disso, implementei soluções de IA para analisar e mapear automaticamente padrões nos dados externos, ajustando-os ao modelo interno para garantir consistência e eficiência na integração. Para suporte aos consultores, utilizei SGBDs (DBeaver), permitindo fluxos ágeis de atualização, inserção, deleção e consulta de dados. Com atalhos internos, foi possível realizar operações em massa sem a necessidade de escrever queries complexas manualmente, tornando o processo mais rápido e seguro. Essas soluções reduziram a necessidade de suporte técnico, otimizaram o fluxo operacional e aprimoraram a escalabilidade do sistema.
             `,
             slug: 'superlogica'
         },
         {
             imageSrc: "/experiences/qualicorp.jpeg",
             imageAlt: "Qualicorp",
-            functionJob: "Desenvolvedor Trainee II Fullstack",
+            functionJob: "Analista Desenvolvedor Trainee II Fullstack",
             enterprise: "Qualicorp",
             schedule: "Tempo integral",
             from: "Fev 2021",
@@ -36,14 +38,16 @@ export default function Experience() {
             place: "São Paulo, Brasil",
             typeJob: "Remoto",
             descritiption: `
-                Atuação no desenvolvimento full-stack com foco em Node.js e Vue.js, incluindo a criação de telas de serviços para o gerenciamento de planos de saúde, com interface intuitiva e funcional. No backend, desenvolvi microserviços utilizando Node.js, otimizando processos internos e garantindo a escalabilidade e a integração eficiente entre diferentes módulos do sistema.
+                Atuei como Analista Desenvolvedor Trainee II Fullstack em um projeto terceirizado para a Qualicorp, focado no desenvolvimento de soluções para o gerenciamento de planos de saúde. Utilizei Vue.js para criar dashboards interativos, permitindo a visualização de contratos, histórico de atualizações da ANS, uso do plano em diferentes períodos e funcionalidades administrativas, como atualização de cadastros, disparo de mensagens e gestão de dados.
+
+                No backend, desenvolvi microserviços em Node.js, estruturando APIs que gerenciavam fluxos específicos do sistema, garantindo escalabilidade e integração eficiente entre módulos. Para armazenar e gerenciar as relações entre os dados, utilizei Neo4J, permitindo atualizações massivas e otimização na gestão das informações dos planos de saúde.
             `,
             slug: 'qualicorp'
         },
         {
             imageSrc: "/experiences/brisa.jpeg",
             imageAlt: "Brisa",
-            functionJob: "Desenvolvedor Júnior",
+            functionJob: "Analista Desenvolvedor Júnior",
             enterprise: "BRISA Tecnologia",
             schedule: "Tempo integral",
             from: "Jun 2019",
@@ -51,7 +55,11 @@ export default function Experience() {
             place: "São Paulo, Brasil",
             typeJob: "Presencial",
             descritiption: `
-                Atuação no desenvolvimento de softwares geral para prestação de serviço a terceiros. Desenvolvimento tanto full-stack de frontend e backend utilizando as mais recentes técnologias do mercado como Node.js, Java Spring Boot, Angular, Vue.js, React, React Native entre outras. Também atuei na automação de serviços para o ramo de hotelaria nos Estados Unidos, para disparo de email, web scraping e gerenciamento de sistemas internos.
+                Atuei como Analista Desenvolvedor Júnior em uma Fábrica de Software que prestava serviços a terceiros, desenvolvendo soluções full-stack para diferentes setores. Utilizei Node.js, Java Spring Boot, Angular, Vue.js, React e React Native para criar sistemas personalizados, além de automações para otimizar operações administrativas.
+
+                No setor de hotelaria, implementei Angular para boilerplates de templates, Vue.js para telas administrativas e Java Spring Boot para APIs de gerenciamento de imóveis. Utilizei Node.js e Express para disparo massivo de e-mails e Selenium e Puppeteer para automações em ERPs e CRMs. Desenvolvi interfaces interativas em React para totens de check-in e aplicativos mobile em React Native para check-in remoto dos clientes.
+
+                No setor de shopping centers, desenvolvi dashboards em React para monitoramento de fluxo de pessoas via câmeras com IA. Criei APIs em Node.js com Express para exibir esses dados e utilizei MongoDB para armazenamento e formatação das informações. Para gerenciar o grande volume de dados captados, implementei um fluxo baseado em AWS (SQS, Lambda), garantindo eficiência no processamento e integração
             `,
             slug: 'brisa'
         }
