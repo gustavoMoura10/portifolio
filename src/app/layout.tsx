a11a348e86da8ae19adbc13a951e5ad17db05b9e
Gustavo Moura <gustavomoura.dev@gmail.com>
2025-03-19 01:42:47 -0300

    fix: Ajuste de layout e inglês

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,23 @@
-import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-
+import { Metadata } from 'next';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Gustavo Moura',
-  description: 'Portfolio de Gustavo Moura',
+    title: "Gustavo Moura",
+    description: "Portfólio de Gustavo Moura",
+    keywords: "Gustavo Moura, Desenvolvedor, Front-end, Back-end, Fullstack, Portfolio, Banco de Dados, Node.js, Java, Javascript, React, Angular, Vue, Express, Next.js, Nest.js, MongoDB, MySQL, TypeScript, PostgreSQL, SQL Server",
 };
-
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html>
-      <body className={inter.className}>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html>
+            <body className={inter.className}>
+                {children}
+            </body>
+        </html>
+    );
 }
