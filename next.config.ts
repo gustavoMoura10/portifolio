999fc535c4ff26662af9ec66e2400d61f8a4c765
Gustavo Moura <gustavo.garcia@totvs.com.br>
2025-12-10 07:42:59 -0300

    fix: Ajuste de versões do novo next-intl

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
+import path from 'path';
 
 const nextConfig: NextConfig = {};
 
-const withNextIntl = createNextIntlPlugin();
+// Força o caminho absoluto
+const withNextIntl = createNextIntlPlugin(
+
+);
+
 export default withNextIntl(nextConfig);
